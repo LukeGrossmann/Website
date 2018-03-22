e0cc5dfd4f7511ed2b6af577afd676156320c6cb
Luke Baziw-Grossmann <Luke@Lukes-Air.hitronhub.home>
2018-03-21 19:37:43 -0700

    added loading script, loader styles, loader in index.pug, made layout.scss more modular by creating a loader.scss, renamed app.js to game.js

---
+++ public/scripts/loading.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".overlay-loading").delay(4000).fadeOut("slow", function () {
+    $('.background').fadeIn('slow');
+  });
+})
+
+// $('.sidebarform').fadeOut('slow', function() {
+//     $('.sidebarsuccess').fadeIn('slow');
+// });
