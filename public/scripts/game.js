0f7ae0b848454bb811a3731bbb1052ed7baf2c76
Luke Baziw-Grossmann <Luke@Lukes-Air.hitronhub.home>
2018-03-24 18:22:17 -0700

    removed commented out ajax that I didn't end up using

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,13 +1,3 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
 /*================================================
 
 Polyfill
