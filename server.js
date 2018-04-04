b938f9f3be53004cddd3adf5bfb62f6460c52d0e
Luke Baziw-Grossmann <Luke@Lukes-Air.hitronhub.home>
2018-04-03 23:29:36 -0700

    comment out example of using a route to load different themes for website

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+// app.get("/scaled_photos/:filename/:width/:height", (req, res) => {
+//   res.render("index");
+// });
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
