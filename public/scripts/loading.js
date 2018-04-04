76a240687f78ec77838e1cc0fe77f1332f3a01b9
Luke Baziw-Grossmann <Luke@Lukes-Air.hitronhub.home>
2018-04-03 23:28:29 -0700

    made loading animation work properly nested it inside of window.load which is now nested inside of document.ready, wrote insatnces of images to load images in tab content that wasn't loading on window.load call, Dom now loads when all images have been loaded

--- public/scripts/loading.js
+++ public/scripts/loading.js
@@ -1,44 +1,71 @@
+$(document).ready(function () {
 
-$(function() {
+  $(".overlay-loading")
 
-  var dt = new Date().toDateString();
-  document.getElementById("datetime").innerHTML = dt;
+  $(window).on("load", function() {
 
-  var d = new Date();
-  var time = d.getHours();
+    function preloadImages(array) {
+        if (!preloadImages.list) {
+            preloadImages.list = [];
+        }
+        var list = preloadImages.list;
+        for (var i = 0; i < array.length; i++) {
+            var img = new Image();
+            img.onload = function() {
+                var index = list.indexOf(this);
+                if (index !== -1) {
+                    // remove image from the array once it's loaded
+                    // for memory consumption reasons
+                    list.splice(index, 1);
+                }
+            }
+            list.push(img);
+            img.src = array[i];
+        }
+    }
 
-  if (time < 12) {
-    document.getElementById("greeting").innerHTML = "Good Morning, User.";
-  }
-  if (time >= 12 && time < 17) {
-    document.getElementById("greeting").innerHTML = "Good Afternoon, User.";
-  }
-  if (time >= 17) {
-    document.getElementById("greeting").innerHTML = "Good Evening, User.";
-  }
+    preloadImages(["../photos/landscape2.jpg", "../photos/panoH@W.jpg", "../photos/blue.jpg"]);
 
-  function firstFadeIn (callback) {
-    $(".overlay-loading").delay(3000).fadeOut("slow", function () {
-      $('.welcome-background').fadeIn('slow');
-        setTimeout(function() {
-        $('.first-text').removeClass('hidden');
-  }, 500);
-      callback();
-    });
-  }
+    var dt = new Date().toDateString();
+    document.getElementById("datetime").innerHTML = dt;
+
+    var d = new Date();
+    var time = d.getHours();
+
+    if (time < 12) {
+      document.getElementById("greeting").innerHTML = "Good Morning, User.";
+    }
+    if (time >= 12 && time < 17) {
+      document.getElementById("greeting").innerHTML = "Good Afternoon, User.";
+    }
+    if (time >= 17) {
+      document.getElementById("greeting").innerHTML = "Good Evening, User.";
+    }
 
 
-  function secondFadeIn () {
-    $('.welcome-background').delay(3500).fadeOut("slow", function () {
-      $('.background-background').fadeIn("slow");
-  $(".remove").remove();
+    function firstFadeIn (callback) {
+      $(".overlay-loading").delay(2000).fadeOut("slow", function () {
+        $('.welcome-background').fadeIn('slow');
+          setTimeout(function() {
+          $('.first-text').removeClass('hidden');
+    }, 500);
+        callback();
+      });
+    }
 
-    });
-  }
 
- firstFadeIn(function() {
-  secondFadeIn();
- });
+    function secondFadeIn () {
+      $('.welcome-background').delay(3500).fadeOut("slow", function () {
+        $('.background-background').fadeIn("slow");
+    $(".remove").remove();
+
+      });
+    }
+
+   firstFadeIn(function() {
+    secondFadeIn();
+   });
+  });
 });
 
 
