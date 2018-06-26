eba49d0cc937f75925abdb35daacd6242db0f77b
Luke Baziw-Grossmann <Luke@Lukes-MacBook-Air.local>
2018-06-26 01:00:10 -0700

    added bio script page, didn't end up using it

---
+++ public/scripts/bio.js
@@ -0,0 +1,31 @@
+// $(document).ready(function () {
+//   var bioIntro = $('.intro, .text-50, .text-10');
+//   if (window.location.href === 'http://localhost:3000/index_spring_summer') {
+
+//     bioIntro.on("mouseenter", function() {
+//       bioIntro.css('background-color', 'wheat');
+//       bioIntro.css('padding', '10px');
+//       bioIntro.css('border', '10px');
+//       bioIntro.css('border-radius', '10px');
+//     });
+//     bioIntro.on('mouseleave', function() {
+//       bioIntro.css('background-color', '');
+//       bioIntro.css('padding', '');
+//       bioIntro.css('border', '');
+//       bioIntro.css('border-radius', '');
+//     });
+//   } else if (window.location.href === 'http://localhost:3000/index_fall_winter') {
+//         bioIntro.on("mouseenter", function() {
+//       bioIntro.css('background-color', 'white');
+//       bioIntro.css('padding', '10px');
+//       bioIntro.css('border', '10px');
+//       bioIntro.css('border-radius', '10px');
+//     });
+//     bioIntro.on('mouseleave', function() {
+//       bioIntro.css('background-color', '');
+//       bioIntro.css('padding', '');
+//       bioIntro.css('border', '');
+//       bioIntro.css('border-radius', '');
+//     });
+//   }
+// });
